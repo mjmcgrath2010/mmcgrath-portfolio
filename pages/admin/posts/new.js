b2f8ee8b297e16f77850195151e454a3ab5aabee
Mike McGrath <mmcgrath@mikes-mbp.lan>
2021-10-04 18:42:35 -0400

    Works

--- pages/admin/posts/new.js
+++ pages/admin/posts/new.js
@@ -1,3 +1,17 @@
-export default () => {
-    return <h1>New</h1>
+import dynamic from "next/dynamic";
+import "easymde/dist/easymde.min.css";
+
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false })
+
+export  default function ReadmeEditor({ value, onChange }) {
+    return (
+        <SimpleMDE
+            options={{
+                spellChecker: false,
+                sideBySideFullscreen: false,
+            }}
+            value={value}
+            onChange={onChange} />
+    );
 }
