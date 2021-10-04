5eb83551f4a9d36102671b175809fbadfdf2bd5c
Mike McGrath <mmcgrath@mikes-mbp.lan>
2021-10-04 19:57:45 -0400

    Add new posts

--- pages/admin/posts/new.js
+++ pages/admin/posts/new.js
@@ -1,17 +1,19 @@
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false })
-
-export  default function ReadmeEditor({ value, onChange }) {
-    return (
-        <SimpleMDE
-            options={{
-                spellChecker: false,
-                sideBySideFullscreen: false,
-            }}
-            value={value}
-            onChange={onChange} />
-    );
+export default function ReadmeEditor({ value, onChange }) {
+  return (
+    <SimpleMDE
+      options={{
+        spellChecker: false,
+        sideBySideFullscreen: false,
+      }}
+      value={value}
+      onChange={onChange}
+    />
+  );
 }
--- pages/api/posts/blog/[pid].js
+++ pages/api/posts/blog/[pid].js
@@ -2,15 +2,27 @@ import dbConnect from "../../../../utils/dbConnect";
 import Post from "../../../../models/Post";
 
 export default async function handler(req, res) {
-    const {method, query: {pid}} = req
-    await dbConnect()
+  const {
+    method,
+    query: { pid },
+  } = req;
+  await dbConnect();
 
-    switch (method) {
-        case 'GET':
-            return Post.find( { slug: `/blog/${pid}` }).then(posts => res.json(posts))
-        case 'POST':
-            return res.status(400).json({success: false});
-        default:
-            return res.status(400).json({success: false})
-    }
+  switch (method) {
+    case "GET":
+      return Post.find({ slug: `/blog/${pid}` }).then((posts) =>
+        res.json(posts)
+      );
+    case "POST":
+      const { description, title, source } = req.body;
+      return Post.create({
+        slug: `/blog/${pid}`,
+        source,
+        description,
+        title,
+      }).then((post) => res.json(post));
+
+    default:
+      return res.status(400).json({ success: false });
+  }
 }
