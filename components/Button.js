920e93c88b3e3ec352679cd78b8d2fe4ac1257ae
Mike McGrath <mike@mikemcgrathonline.us>
2021-10-04 20:45:28 -0400

    start on storybook

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
   background: ${({ theme, color }) => theme.colors[color]};
 `;
 
-const Button = ({ text, color, variant }) => (
+const Button = ({ text, color, onClick, variant }) => (
   <StyledButton color={color} variant={variant}>
     {text}
   </StyledButton>
 );
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+};
 
 export default Button;
---
+++ stories/Button.stories.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Button from "../components/Button";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/Button",
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    color: { control: "color" },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  color: undefined,
+  text: "Button",
+};
