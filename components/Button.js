61108af76f6baecdf0a265ffcb83a88cceeba0eb
Mike McGrath <mike@mikemcgrathonline.us>
2021-10-04 20:36:27 -0400

    WOrking

--- components/Button.js
+++ components/Button.js
@@ -1,1 +1,14 @@
-const Button = ({}) => {};
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background: ${({ theme, color }) => theme.colors[color]};
+`;
+
+const Button = ({ text, color, variant }) => (
+  <StyledButton color={color} variant={variant}>
+    {text}
+  </StyledButton>
+);
+
+export default Button;
